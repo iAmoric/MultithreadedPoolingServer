a77ad8843676a468bc2ee0f4ca3908818009b09a
iamoric <pierratlucas@gmail.com>
2018-06-19 23:33:07 +0200

    Create httpServer with thread pool

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,22 @@
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
 
     private static final int DEFAULT_PORT = 8080;
 
     public static void main(String[] args) {
         int port = DEFAULT_PORT;
-
+        HttpServer httpServer = null;
+        ExecutorService executor = null;
+        
         // dynamically defines the number of threads
         int max_thread = Runtime.getRuntime().availableProcessors();
+        executor = Executors.newFixedThreadPool(max_thread);
 
         if (args.length > 0) {
             try {
@@ -22,6 +32,23 @@ public class Main {
         else {
             System.out.println("No port specify. Use default port (" + DEFAULT_PORT + ")");
         }
+        
+        try {
+            httpServer = HttpServer.create(new InetSocketAddress(port), 0);
+        } catch (IOException e) {
+            System.err.println("Unable to create Http server on port " + port);
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        // ensure that httpServer has been created
+        assert (httpServer != null);
+        // TODO: create RequestHandler class
+        // httpServer.createContext("/", new RequestHandler());
+        httpServer.setExecutor(executor);
+        httpServer.start();
+
+        System.out.println("Server listening on port " + port + "...");
 
     }
 }
