7e45c4314f350629d2d97cef54d8ec1622b2a942
iamoric <pierratlucas@gmail.com>
2018-06-20 22:18:45 +0200

    Get keep-alive header

--- src/RequestHandler.java
+++ src/RequestHandler.java
@@ -1,9 +1,16 @@
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-import java.io.OutputStream;
+
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
 
 public class RequestHandler implements HttpHandler {
+
+    private String response = "Hello, World! from the server\n";
+    private static int STATUS_OK = 200;
+    private boolean keepAlive = false;
 
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
@@ -15,7 +22,19 @@ public class RequestHandler implements HttpHandler {
             return;
         }
 
-        // TODO: get
+        // Get Keep-alive header
+        Headers headers = httpExchange.getRequestHeaders();
+        for (String key : headers.keySet()) {
+            if (key.equalsIgnoreCase("Connection")) {
+                for (String value : headers.get(key)) {
+                    if (value.equalsIgnoreCase("Keep-Alive")) {
+                        keepAlive = true;
+                    }
+                }
+            }
+        }
+        System.out.println("Persistent connection: " + keepAlive);
+
         String method = httpExchange.getRequestMethod();
 
         switch (method) {
@@ -39,6 +58,4 @@ public class RequestHandler implements HttpHandler {
     private void handlePostMethod(HttpExchange httpExchange) throws IOException {
         // TODO: parse URL to get params
     }
-
-
 }
