1f6ba38496aafa2d6ce973b65f091bce937c88d7
iamoric <pierratlucas@gmail.com>
2018-06-20 23:35:28 +0200

    Use local variables and close connection if no keep-alive

--- src/RequestHandler.java
+++ src/RequestHandler.java
@@ -4,17 +4,18 @@ import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.List;
+import java.net.URI;
 
 public class RequestHandler implements HttpHandler {
 
-    private String response = "Hello, World! from the server\n";
+
     private static int STATUS_OK = 200;
-    private boolean keepAlive = false;
 
     @Override
+    // Only use local variable to be thread safe
     public void handle(HttpExchange httpExchange) throws IOException {
-        System.out.println("Handle request");
+        String clientAddr = httpExchange.getRemoteAddress().getHostString();
+        System.out.println("\nHandle request for " + clientAddr);
 
         if (!httpExchange.getProtocol().equalsIgnoreCase("HTTP/1.1")){
             System.err.println("Unsupported protocol");
@@ -23,6 +24,7 @@ public class RequestHandler implements HttpHandler {
         }
 
         // Get Keep-alive header
+        boolean keepAlive = false;
         Headers headers = httpExchange.getRequestHeaders();
         for (String key : headers.keySet()) {
             if (key.equalsIgnoreCase("Connection")) {
@@ -35,8 +37,16 @@ public class RequestHandler implements HttpHandler {
         }
         System.out.println("Persistent connection: " + keepAlive);
 
-        String method = httpExchange.getRequestMethod();
+        // Get URI
+        // TODO
+        /*URI uri = httpExchange.getRequestURI();
+        String response = "Hello, World! You requested " + uri.getPath() + "\n";
+        httpExchange.sendResponseHeaders(STATUS_OK, response.length());
+        OutputStream os = httpExchange.getResponseBody();
+        os.write(response.getBytes());*/
 
+        // Get Method
+        String method = httpExchange.getRequestMethod();
         switch (method) {
             case "GET":
                 handleGetMethod(httpExchange);
@@ -47,6 +57,12 @@ public class RequestHandler implements HttpHandler {
             default:
                 System.err.println("\tUnsupported Http method: " + method);
                 break;
+        }
+
+        // Close connection if there is no Keep-Alive header
+        if (!keepAlive) {
+            System.out.println("Closing connection of " + clientAddr);
+            httpExchange.close();
         }
     }
 
