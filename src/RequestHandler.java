2b3514927698c37f7fb67b8a85cfcf6aebd99d58
iAmoric <pierratlucas@gmail.com>
2018-06-22 14:41:11 +0200

    Handle GET resquest

--- src/RequestHandler.java
+++ src/RequestHandler.java
@@ -2,9 +2,10 @@ import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.URI;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,6 +13,8 @@ public class RequestHandler implements HttpHandler {
 
 
     private static int STATUS_OK = 200;
+    private static int STATUS_NOT_FOUND = 404;
+    private static String WEB_ROOT_DIRECTORY = System.getProperty("user.dir") + "/web";
 
     @Override
     // Only use local variable to be thread safe
@@ -38,27 +41,20 @@ public class RequestHandler implements HttpHandler {
         }
         System.out.println("Persistent connection: " + keepAlive);
 
-        //
-        String path = httpExchange.getRequestURI().getPath();
-
         // Get Query parameterss
         String paramString = httpExchange.getRequestURI().getRawQuery();
-        Map<String, String> paramsMap = getParams(paramString);
-        System.out.println("Query parameters:");
-        for (Map.Entry<String, String> e : paramsMap.entrySet()) {
-            System.out.println("\t" + e.getKey() + ": " + e.getValue());
+        if (paramString != null) {
+            System.out.println(paramString);
+            Map<String, String> paramsMap = getParams(paramString);
+            System.out.println("Query parameters:");
+            for (Map.Entry<String, String> e : paramsMap.entrySet()) {
+                System.out.println("\t" + e.getKey() + ": " + e.getValue());
+            }
         }
-
-        // Get URI
-        // TODO
-        /*URI uri = httpExchange.getRequestURI();
-        String response = "Hello, World! You requested " + uri.getPath() + "\n";
-        httpExchange.sendResponseHeaders(STATUS_OK, response.length());
-        OutputStream os = httpExchange.getResponseBody();
-        os.write(response.getBytes());*/
 
         // Get Method
         String method = httpExchange.getRequestMethod();
+        System.out.println(method);
         switch (method) {
             case "GET":
                 handleGetMethod(httpExchange);
@@ -78,16 +74,51 @@ public class RequestHandler implements HttpHandler {
         }
     }
 
+    /**
+     *
+     * @param httpExchange
+     * @throws IOException
+     */
+    private synchronized void handleGetMethod(HttpExchange httpExchange) throws IOException{
+        // Get requested path
+        String path = httpExchange.getRequestURI().getPath();
+        OutputStream os = httpExchange.getResponseBody();
 
-    private void handleGetMethod(HttpExchange httpExchange) {
-        // TODO: send content
+        // Get file for the response
+        File file = getFile(path);
+        if (file == null) {
+            String response = path + " : Page not found\n";
+            httpExchange.sendResponseHeaders(STATUS_NOT_FOUND, response.length());
+            os.write(response.getBytes());
+        }
+        else {
+            // Add content-type to the response header
+            Headers header = httpExchange.getResponseHeaders();
+            header.add("Content-type", "text/html");
+
+            // Read content of the file
+            BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+            byte[] b  = new byte[(int)file.length()];
+            bis.read(b, 0, b.length);
+
+            // Send content to the client
+            httpExchange.sendResponseHeaders(STATUS_OK, file.length());
+            os.write(b,0, b.length);
+        }
+        os.flush();
+        os.close();
     }
 
-    private void handlePostMethod(HttpExchange httpExchange) throws IOException {
+    private synchronized void handlePostMethod(HttpExchange httpExchange) throws IOException {
         // TODO: parse URL to get params
     }
 
-    private Map<String, String> getParams(String paramsString) {
+    /**
+     * Return a Map of every parameters in the request paramsString
+     * @param paramsString
+     * @return paramsMap
+     */
+    private synchronized Map<String, String> getParams(String paramsString) {
         String paramDelimiter = "&";
         String valueDelimiter = "=";
 
@@ -104,5 +135,19 @@ public class RequestHandler implements HttpHandler {
             }
         }
         return paramsMap;
+    }
+
+    /**
+     * Get and return a file at a given path
+     * @param path
+     * @return File if the file has been found, or null otherwise
+     */
+    private File getFile(String path) {
+        File f = new File(WEB_ROOT_DIRECTORY, path);
+
+        if(f.exists() && !f.isDirectory()) {
+            return f;
+        }
+        return null;
     }
 }
